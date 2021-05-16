540a4a079504e6473b77b625ec73d90fc8c00cf4
Lamaney <omerffarukaydin@gmail.com>
2021-05-17 00:40:30 +0300

    Uzak veritabanına bağlanıldı.

--- Isg_Mvc/Models/BlogContext.cs
+++ Isg_Mvc/Models/BlogContext.cs
@@ -7,6 +7,13 @@ namespace Isg_Mvc.Models{
 
         public BlogContext(DbContextOptions<BlogContext> options): base(options){}
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+
+            optionsBuilder.UseSqlServer("server=77.245.159.23\\MSSQLSERVER2019;Database=ISG_DB;user Id=isg_odev;Password=Tx?9io52;");
+
+        }
+
+
         public DbSet<Blog>Blog{get;set;}
 
         public DbSet<Author>Author{get;set;}
